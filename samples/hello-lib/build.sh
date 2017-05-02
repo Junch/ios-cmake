d48efa942f70bab2cf82141df2003e90cbfb8609
Jun Chen <junc76@gmail.com>
2017-05-02 14:19:02 +0800

    Make a universal library with lipo

--- samples/hello-lib/build.sh
+++ samples/hello-lib/build.sh
@@ -1,6 +1,14 @@
 mkdir _build > /dev/null
 pushd _build > /dev/null
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../../../toolchain/ios.cmake -DIOS_PLATFORM=OS
-make
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../../../toolchain/ios.cmake -DIOS_PLATFORM=SIMULATOR64
 make install
 popd > /dev/null
+
+mkdir _buildos > /dev/null
+pushd _buildos > /dev/null
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../../../toolchain/ios.cmake -DIOS_PLATFORM=OS
+make install
+popd > /dev/null
+
+lipo -create ./_build/libhello-lib.a ./_buildos/libhello-lib.a -output ../hello-app/hello-app/libhello-lib.a
+lipo -info ../hello-app/hello-app/libhello-lib.a
